00300185f787e77c0023de8608cad294d4bbf85d
Hunter Sparrow <HunterSparrow@hunters-macbook-pro.local>
2018-03-08 22:24:28 +0900

    Initial Commit

---
+++ RecordMe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RecordMe
+//
+//  Created by Hunter Sparrow on 3/8/18.
+//  Copyright © 2018 Hunter Sparrow. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
